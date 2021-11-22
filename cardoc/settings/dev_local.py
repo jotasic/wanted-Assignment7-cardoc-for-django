0d29d97312ced6b45309d64a71f0103d297596cf
TaewooKim <embedded61@gmail.com>
2021-11-22 16:44:10 +0900

    Add: 프로젝트 생성 및 환경설정 - django 프로젝트 생성 및 설정 값 변경 - docker 개발 환경 설정

---
+++ cardoc/settings/dev_local.py
@@ -0,0 +1,18 @@
+from .base import *
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = True
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django_secret_key')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('SQL_DATABASE_NAME', 'cardoc_local_dev'),
+        'USER': os.environ.get('SQL_USER', 'cardoc_local_dev'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', 'cardoc_local_dev'),
+        'HOST': os.environ.get('SQL_HOST', 'localhost'),
+        'PORT': os.environ.get('SQL_PORT', 5432),
+    }
+}
