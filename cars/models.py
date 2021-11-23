fe47fb7d5dd52d4c69b947ece8d901e5d210246d
TaewooKim <embedded61@gmail.com>
2021-11-23 20:52:50 +0900

    Modify: 타이어 스펙 저장부분 수정 - 기존에 width 부분에 P가 들어와도 같이 저장했는데, P가 들어오면 빼고   저장되도록 수정함 - 이에 따라서 타이어 관련 스펙을 저장하는 필드는 charField에서   intggerField로 변경

--- cars/models.py
+++ cars/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Tire(models.Model):
     pk_id        = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
-    width        = models.CharField(max_length=4)
-    aspect_ratio = models.CharField(max_length=2)
-    wheel_size   = models.CharField(max_length=2)
+    width        = models.PositiveSmallIntegerField()
+    aspect_ratio = models.PositiveSmallIntegerField()
+    wheel_size   = models.PositiveSmallIntegerField()
     created_at   = models.DateTimeField(auto_now=True)
     updated_at   = models.DateTimeField(auto_now_add=True)
     is_actvate   = models.BooleanField(default=True)
